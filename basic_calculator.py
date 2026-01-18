b35abb764a29e69338377adfbd97f2620c3b9bf4
Anmol Malhotra <anmolmalhotra267@gmail.com>
2026-01-18 18:02:27 +0530

    Basic Calculator
    Added comments to clarify user input and operation choices.

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,18 +1,18 @@
 print("----CALCULATOR----")
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+num1 = int(input("Enter first number: "))       #Input first number
+num2 = int(input("Enter second number: "))      #Input second number
 print("--Operations--")
 print("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Power")
-choice = int(input("Enter your choice as mentioned above(1 - 5): "))
-if (choice == 1):
+choice = int(input("Enter your choice as mentioned above(1 - 5): "))   #choose operation 
+if (choice == 1):                                    # If choice is 1 → Addition
     print(num1, "+", num2, "=", num1 + num2)
-elif (choice == 2):
+elif (choice == 2):                                  # If choice is 2 → Subtraction
     print(num1, "-", num2, "=", num1 - num2)
-elif (choice == 3):
+elif (choice == 3):                                  # If choice is 3 → Multiplication
     print(num1, "*", num2, "=", num1 * num2)
-elif (choice == 4):
+elif (choice == 4):                                  # If choice is 4 → Division
     print(num1, "/", num2, "=", num1 / num2)
-elif (choice == 5):
+elif (choice == 5):                                  # If choice is 5 → Power (num1 raised to num2)
     print(num1, "^", num2, "=", num1 ** num2)
-else:
+else:                                                # If user enters an invalid choice
     print("Invalid choice. Enter number from 1 to 5 as per your choice.")
